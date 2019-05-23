5cec34f72def2d31a07db97f9e6da5c001855ca6
Dimitri.Finger <dimitri.finger@acad.pucrs.br>
2019-05-23 19:12:40 -0300

    Version 2.0 - in python

--- MarsRovers.py
+++ MarsRovers.py
@@ -1,17 +1,46 @@
-string_entrada = "5 5 1 2 N LMLMLMLMM 3 3 E MMRMMRMRRM"
+string_entrada = "5 5 1 2 N RRMM"
 new_string = string_entrada.split(" ")
-print(new_string)
+
 
 if int(new_string[0])>=0:
-    map_x= new_string[0]
+    map_size_x = int(new_string[0])
 if int(new_string[1])>=0:
-    map_y = new_string[1]
+    map_size_y = int(new_string[1])
 
-initial_pos_x = new_string[2]
-initial_pos_y = new_string[3]
+pos_x = int(new_string[2])
+pos_y = int(new_string[3])
+facing = new_string[4]
+print("Initial position : " + str(pos_x) + " " + str(pos_y) + " " + facing)
+for commands in new_string[5]:
+    if facing == "N" and commands == "L":
+        facing = "W"
+    elif facing == "N" and commands == "R":
+        facing = "E"    
+    elif facing == "S" and commands == "L":
+        facing = "E"
+    elif facing == "S" and commands == "R":
+        facing = "W"    
+    elif facing == "W" and commands == "L":
+        facing = "S"
+    elif facing == "W" and commands == "R":
+        facing = "N" 
+    elif facing == "E" and commands == "L":
+        facing = "N"
+    elif facing == "E" and commands == "R":
+        facing = "S"
+
+    if commands == "M" and facing=="N":        
+        if pos_y < map_size_y:
+            pos_y += 1        
+    if commands == "M" and facing=="S":
+        if pos_y > 0:
+            pos_y -= 1 
+    if commands == "M" and facing=="E":
+       if pos_x < map_size_x:
+            pos_x += 1 
+    if commands == "M" and facing=="W":
+        if pos_x > 0:
+            pos_x -= 1           
 
 
-
-
-print(map_x + " " + map_y)
-print(initial_pos_x + " " + initial_pos_y)
+print("Final position: " + str(pos_x) + " " + str(pos_y) + " " + facing)
