33d4c84954be7ae05afe4af3b650a17ffff55f4b
DIMITRI DA SILVA FINGER <11104920@pucrs.br>
2019-05-23 20:39:27 -0300

    Tuples added.    WIP

--- MarsRovers.py
+++ MarsRovers.py
@@ -1,4 +1,4 @@
-def process_movement(movements, facing, pos_x, pos_y, map_size_x, map_size_y):
+def process_movement(movements, facing, position, field):
     for commands in movements:
         if facing == "N" and commands == "L":
             facing = "W"
@@ -18,31 +18,30 @@ def process_movement(movements, facing, pos_x, pos_y, map_size_x, map_size_y):
             facing = "S"
 
         if commands == "M" and facing=="N":        
-            if pos_y < map_size_y:
-                pos_y += 1        
+            if position[1] < field[1]:
+                position[0] += 1        
         if commands == "M" and facing=="S":
-            if pos_y > 0:
-                pos_y -= 1 
+            if position[1] > 0:
+                position[1] -= 1 
         if commands == "M" and facing=="E":
-            if pos_x < map_size_x:
-                pos_x += 1 
+            if position[0] < field[0]:
+                position[0] += 1 
         if commands == "M" and facing=="W":
-            if pos_x > 0:
-                pos_x -= 1
-    return print("Final position: " + str(pos_x) + " " + str(pos_y) + " " + facing)           
+            if position[0] > 0:
+                position[0] -= 1
+    return print("Final position: " + str(position[0]) + " " + str(position[1]) + " " + facing)           
 
 def rover(entry_string):
     new_string = entry_string.split(" ")
 
-    if int(new_string[0])>=0 and int(new_string[1])>=0:
-        map_size_x = int(new_string[0])
-        map_size_y = int(new_string[1])      
-        
-    pos_x = int(new_string[2])
-    pos_y = int(new_string[3])
+    if int(new_string[0])>=0 and int(new_string[1])>=0:      
+        field = (int(new_string[0]),int(new_string[1])) 
+
+    position=(int(new_string[2]), int(new_string[3]))
+    
     facing = new_string[4]
-    print("Initial position : " + str(pos_x) + " " + str(pos_y) + " " + facing)
-    process_movement(new_string[5], facing, pos_x, pos_y, map_size_x, map_size_y)
+    print("Initial position : " + str(position[0]) + " " + str(position[1]) + " " + facing)
+    process_movement(new_string[5], facing, position, field)
 
 rover("5 5 1 2 N MM")
 rover("5 5 1 2 N MMRMMRMM")
