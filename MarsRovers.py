8f520ecd70a2dab3a80e22a0c154f10c00d02aef
DimitriFinger <dimitri.finger@acad.pucrs.br>
2019-05-24 17:08:32 -0300

    Fixing Tuples - WIP

--- MarsRovers.py
+++ MarsRovers.py
@@ -19,16 +19,16 @@ def process_movement(movements, facing, position, field):
 
         if commands == "M" and facing=="N":        
             if position[1] < field[1]:
-                position[0] += 1        
+                position=(position[0], position[1]+1)        
         if commands == "M" and facing=="S":
             if position[1] > 0:
-                position[1] -= 1 
+                position=(position[0], position[1]-1) 
         if commands == "M" and facing=="E":
             if position[0] < field[0]:
-                position[0] += 1 
+                position = (position[0] + 1, position[1]) 
         if commands == "M" and facing=="W":
             if position[0] > 0:
-                position[0] -= 1
+               position=(position[0] - 1, position[1])
     return print("Final position: " + str(position[0]) + " " + str(position[1]) + " " + facing)           
 
 def rover(entry_string):
@@ -37,7 +37,7 @@ def rover(entry_string):
     if int(new_string[0])>=0 and int(new_string[1])>=0:      
         field = (int(new_string[0]),int(new_string[1])) 
 
-    position=(int(new_string[2]), int(new_string[3]))
+    position=(new_string[2], new_string[3])
     
     facing = new_string[4]
     print("Initial position : " + str(position[0]) + " " + str(position[1]) + " " + facing)
